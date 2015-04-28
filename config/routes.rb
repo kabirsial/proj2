5ca1a9df4564df4f1bfdbd23958783ade74f4d28
vinaysatish <satish.vinay@berkeley.edu>
2015-04-28 13:08:50 -0700

    Added User, Admin, Article Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
+  resources :articles
 
+  devise_for :admins
+  devise_for :users
+  get 'home', to: 'home/index'
   root to: 'home#index'
-  get 'home/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
